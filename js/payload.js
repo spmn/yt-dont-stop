892ff678ac68375dab39921aa73b59d1f28c8f2f
spmn <spmn@users.noreply.github.com>
2019-06-23 15:09:14 +0300

    v1.1.0

--- js/payload.js
+++ js/payload.js
@@ -4,7 +4,7 @@
 // @homepageURL  https://github.com/spmn/yt-dont-stop/
 // @downloadURL  https://raw.githubusercontent.com/spmn/yt-dont-stop/master/js/payload.js
 // @icon         https://raw.githubusercontent.com/spmn/yt-dont-stop/master/img/icon_128.png
-// @version      1.0.0
+// @version      1.1.0
 // @description  Prevents annoying "video paused" dialogs from showing up
 // @author       spmn
 // @match        *://*.youtube.com/*
